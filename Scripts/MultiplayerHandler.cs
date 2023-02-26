38f19cd754e7bce44b749b9aa9585df63c6969bf
JonDev2023 <roupasee@gmail.com>
2023-02-26 13:04:59 -0300

    Level 0 Change Multiplayer

--- Scripts/MultiplayerHandler.cs
+++ Scripts/MultiplayerHandler.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Threading;
 using UnityEngine;
 
 public class MultiplayerHandler : MonoBehaviour
 {
+
+    public bool multiplayer;
+    public string FileDataPath;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    void GPU(string txt)
+    {
+        Debug.Log(txt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown (KeyCode.KeypadEnter)) {
+            Debug.Log("2P Activated");
+            multiplayer = true;
+            try {
+                using var file = File.CreateText(FileDataPath);
+                file.WriteLine(multiplayer.ToString());
+                file.Close();
+            } catch (Exception ex) {
+                GPU("Sorry! Error there");
+                multiplayer = false;
+                Thread.Sleep(1000 * 10);
+            }
+       }
     }
 }
