8ae6ade3dbff7b192d794e12873803fd0e213fbb
JonDev2023 <roupasee@gmail.com>
2023-02-26 23:25:22 -0300

    Void Added

--- Scripts/MultiplayerHandler.cs
+++ Scripts/MultiplayerHandler.cs
@@ -4,47 +4,31 @@ using System.IO;
 using System;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MultiplayerHandler : MonoBehaviour
 {
-
     public bool multiplayer;
-    public string FileDataPath;
-
-    void GPU(string txt)
-    {
-        Debug.Log(txt);
-    }
+    public GameObject player2;
 
     // Start is called before the first frame update
     void Start()
     {
-        try {
-            using var file = File.CreateText(FileDataPath);
-            file.WriteLine(multiplayer.ToString());
-            file.Close();
-        } catch (Exception ex) {
-            GPU("Sorry! Error there");
-            multiplayer = false;
-            Thread.Sleep(1000 * 10);
-        }
+        player2.SetActive(false);
+    }
+
+    // Multiplayer is called when the enter on keypad is pressed
+    void Multiplayer()
+    {
+        player2.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
        if (Input.GetKeyDown (KeyCode.KeypadEnter)) {
-            Debug.Log("2P Activated");
             multiplayer = true;
-            try {
-                using var file = File.CreateText(FileDataPath);
-                file.WriteLine(multiplayer.ToString());
-                file.Close();
-            } catch (Exception ex) {
-                GPU("Sorry! Error there");
-                multiplayer = false;
-                Thread.Sleep(1000 * 10);
-            }
+            Multiplayer();
        }
     }
 }
